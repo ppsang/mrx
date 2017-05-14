65f040adc936d375fc9ab95f8ea2892d2d3b14a6
brad <chenhongvvei@gmail.com>
2017-05-14 15:59:38 +0800

    sdfa

--- space.mrx.basic/space.mrx.basic.web/src/main/java/space/mrx/basic/web/screen/Welcome.java
+++ space.mrx.basic/space.mrx.basic.web/src/main/java/space/mrx/basic/web/screen/Welcome.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Welcome {
 
 	@RequestMapping("/welcome/{param}")
-	public String wlecome(HttpServletRequest request, HttpServletResponse response, @PathVariable String param){
+	public String wlecome(@PathVariable String param, HttpServletRequest request, HttpServletResponse response, Model model){
 		
+		// model.addAttribute("afa","1");
 		System.out.println("传递进来的参数是：" + param);
+		
 		// 这儿返回的东西，会自动加上前缀和后缀进行匹配 会自动进行跳转
 		return "index";
 	}
